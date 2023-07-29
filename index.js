0c8107b9d7f70e2e121519f17e12c4c798ac28e3
ruben-abreu <110782140+ruben-abreu@users.noreply.github.com>
2023-07-29 21:41:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function playDiceGame() {
 let min = 1;
 let max = 6;
 let randomNumber1 = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,5 +13,8 @@ if(randomNumber1>randomNumber2){
 }else if (randomNumber2>randomNumber1){
     document.querySelector('h1').innerHTML="Player2 wins! 🚩";}
     else{document.querySelector('h1').innerHTML=" Draw! 🚩";};
+}
 
 
+const button = document.getElementById("start");
+button.addEventListener("click", playDiceGame);
