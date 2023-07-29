b158e3b9fb3b14367bd198227a47c1e614f3a78f
ruben-abreu <110782140+ruben-abreu@users.noreply.github.com>
2023-07-29 12:33:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let min = 1;
+let max = 6;
+let randomNumber1 = Math.floor(Math.random() * (max - min + 1)) + min;
+let randomNumber2 = Math.floor(Math.random() * (max - min + 1)) + min;
+
+document.querySelector(".img1").src = `./images/dice${randomNumber1}.png`;
+document.querySelector(".img2").src = `./images/dice${randomNumber2}.png`;
+  
+
+if(randomNumber1>randomNumber2){
+    document.querySelector('h1').innerHTML="Player1 wins! 🚩";
+}else if (randomNumber2>randomNumber1){
+    document.querySelector('h1').innerHTML="Player2 wins! 🚩";}
+    else{document.querySelector('h1').innerHTML=" Draw! 🚩";};
+
+
